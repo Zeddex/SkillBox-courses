8be446fe360ed7e75e375c42655ca92764bbef92
Zed <31566989+Zeddex@users.noreply.github.com>
2020-02-18 23:14:12 +0100

    Add files via upload

---
+++ Homework_9/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace Homework_9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region HW9
+            // Создать бота, позволяющего принимать разные типы файлов, 
+            // *Научить бота отправлять выбранный файл в ответ
+            // 
+            // http://t.me/BotFather
+            // @BotFather
+            //
+            #endregion
+
+            BotCore.BotInit();          // Starting Telegram.Bot
+        }
+    }
+}
