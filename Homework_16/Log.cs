fc3c2e4c7bcc7d0e1b1fa7bfac4bc6c31d7652ed
Zeddex <zedxlab@gmail.com>
2021-03-22 00:58:44 +0100

    hw 15 finished

---
+++ Homework_16/Log.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace Homework_16
+{
+    public class Log
+    {
+        public ObservableCollection<string> logFile = new ObservableCollection<string>();
+
+        /// <summary>
+        /// Add message to log list
+        /// </summary>
+        /// <param name="msg"></param>
+        public void AddToLog(string msg)
+        {
+            logFile.Add(msg);
+        }
+    }
+}
