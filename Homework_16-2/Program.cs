67d9916e7d55593c78f83702768ab284d7de37b3
Zeddex <zedxlab@gmail.com>
2021-04-05 22:43:47 +0200

    HW16-3 added

---
+++ Homework_16-2/Program.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Management;
+
+namespace Homework_16_2
+{
+    class Program
+    {
+        #region HW 16-2
+
+        // Вычислить количество чисел на промежутке 
+        // от 1 000 000 000
+        // до 2 000 000 000
+        // в которых сумма цифр числа кратна последней цифре числа
+
+        // Число        Сумма цифр      Последняя цифра     Кратность
+        //    11                 2                    1            Да    
+        //    12                 3                    2           Нет     
+        //    19                10                    9           Нет      
+        //    20                 2                    0            Да      
+        //   123                 6                    3            Да       
+        //                                                           
+        // 
+        #endregion
+
+        public static int result;
+
+        static void Main(string[] args)
+        {
+            int startNumber = 1_000_000_000;
+            int endNumber = 1_100_000_000;
+
+            Console.WriteLine("Cores information:");
+
+            int coreCount = 0;
+            foreach (var item in new ManagementObjectSearcher("Select * from Win32_Processor").Get())
+            {
+                coreCount += int.Parse(item["NumberOfCores"].ToString());
+            }
+            Console.WriteLine($"Physical cores = {coreCount}");
+
+            int threadsCount = Environment.ProcessorCount;
+            Console.WriteLine($"Logical cores = {threadsCount}");
+
+            int workingCores = threadsCount - 1;
+
+            int perCore = (endNumber - startNumber) / workingCores;
+            Console.WriteLine($"Working cores = {workingCores}\n");
+
+            var tasks = new Task[workingCores];
+
+            // 1 thread mode
+            DateTime startTime = DateTime.Now;
+            Calculate(startNumber, endNumber);
+            TimeSpan span = DateTime.Now.Subtract(startTime);
+
+            Console.WriteLine($"Numbers from {startNumber} to {endNumber}");
+            Console.WriteLine($"Amount of multiple numbers: {result}\n");
+            Console.WriteLine($"Execution time in 1 thread mode: {span.TotalSeconds} s\n");        // 1bil - 118s, 100mln - 12s
+
+            // multi thread mode
+            Console.WriteLine("Multi thread mode:");
+            result = 0;
+            startTime = DateTime.Now;
+
+            //var outerTask = Task.Run(() =>
+            //{
+            for (int i = 0; i < workingCores; i++)
+            {
+                int fromNumb = startNumber;
+                int toNumb = startNumber + perCore;
+
+                Console.WriteLine($"Process: {i + 1}. Numbers from {fromNumb} to {toNumb}");
+                tasks[i] = Task.Run(() => Calculate(fromNumb, toNumb));
+
+                startNumber += perCore;
+
+                //tasks[i].Wait();
+            }
+            //});
+            //outerTask.Wait();
+
+            foreach (var task in tasks)
+            {
+                task.Wait();
+            }
+
+            //Task.WaitAll();
+
+            TimeSpan span2 = DateTime.Now.Subtract(startTime);
+
+            Console.WriteLine($"\nAmount of multiple numbers: {result}\n");
+            Console.WriteLine($"Execution time in multi thread mode: {span2.TotalSeconds} s");    // 1bil - 21s, 100mln - 2s
+
+            Console.ReadLine();
+        }
+
+        public static void Calculate(int start, int end)
+        {
+            int sum, currNumb, lastDigit;
+
+            for (int i = start; i <= end; i++)
+            {
+                sum = 0;
+                currNumb = i;
+
+                while (currNumb != 0)
+                {
+                    sum += currNumb % 10;
+                    currNumb /= 10;
+                }
+
+                lastDigit = i % 10;
+                //Console.WriteLine($"Number {i}, sum of all digits {sum}, last digit {lastDigit}");
+
+                if (lastDigit == 0 || lastDigit == 1 || sum % lastDigit == 0)
+                    result++;
+            }
+
+        }
+    }
+}
---
+++ Homework_16-3/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Management;
+using System.Threading.Tasks;
+
+namespace Homework_16_3
+{
+    class Program
+    {
+        #region HW 16-3
+        // Написать программу перемножения матриц используя многопоточность
+        #endregion
+
+        public static Random rnd = new();
+
+        public static int x1 = 3000;
+        public static int y1 = 2000;
+        public static int x2 = 2000;
+        public static int y2 = 3000;
+        public static int[,] matrix1 = new int[y1, x1];
+        public static int[,] matrix2 = new int[y2, x2];
+        public static int[,] matrix3 = new int[y1, x2];
+        public static int cycle = 0;
+
+        static void Main(string[] args)
+        {
+            int iteration = y1 < y2 ? y1 : y2;
+
+            // 1st matrix
+            for (int i = 0; i < y1; i++)
+            {
+                for (int k = 0; k < x1; k++)
+                {
+                    matrix1[i, k] = rnd.Next(1, 6);
+                }
+            }
+
+            // 2nd matrix
+            for (int i = 0; i < y2; i++)
+            {
+                for (int k = 0; k < x2; k++)
+                {
+                    matrix2[i, k] = rnd.Next(1, 6);
+                }
+            }
+
+            // Result matrix
+
+            // 1 thread mode
+            DateTime startTime = DateTime.Now;
+
+            while (iteration != 0)
+            {
+                CalcMatrix(0);
+                iteration--;
+            }
+
+            TimeSpan span = DateTime.Now.Subtract(startTime);
+            Console.WriteLine($"Execution time in 1 thread mode: {span.TotalSeconds} s");         // 187s
+
+            // Multi thread mode
+            cycle = 0;
+            iteration = y1 < y2 ? y1 : y2;
+
+            startTime = DateTime.Now;
+
+            Parallel.For(0, iteration, CalcMatrix);
+
+            TimeSpan span2 = DateTime.Now.Subtract(startTime);
+            Console.WriteLine($"Execution time in multi thread mode: {span2.TotalSeconds} s");     // 41s
+
+            Console.ReadLine();
+        }
+
+        public static void CalcMatrix(int x)
+        {
+            for (int k = 0; k < x1 && k < x2; k++)
+            {
+                for (int j = 0; j < x1; j++)
+                {
+                    matrix3[cycle, k] += matrix1[k, j] * matrix2[j, cycle];
+                }
+            }
+            cycle++;
+        }
+    }
+}
