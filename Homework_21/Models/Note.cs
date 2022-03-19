d1c92c143e7f911f39508dd5c7305e654f29eb91
Zeddex <zedxlab@gmail.com>
2022-03-20 00:49:40 +0100

    add db

---
+++ Homework_21/Models/Note.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Homework_21.Models
+{
+    public class Note
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "No name")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "No surname")]
+        public string Surname { get; set; }
+
+        [Required(ErrorMessage = "No phone")]
+        public string Phone { get; set; }
+
+        [Required(ErrorMessage = "No address")]
+        public string Address { get; set; }
+
+        [Required(ErrorMessage = "No IBAN")]
+        public string Iban { get; set; }
+    }
+}
