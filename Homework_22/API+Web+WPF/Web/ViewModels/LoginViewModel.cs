13d7b821d87fa0c9c9bc4af56b296b09546b56ac
Zeddex <zedxlab@gmail.com>
2022-04-03 22:02:27 +0200

    refactoring

---
+++ Homework_22/API+Web+WPF/Web/ViewModels/LoginViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Homework_22.ViewModels
+{
+    public class LoginViewModel
+    {
+        [Required(ErrorMessage = "Please enter login")]
+        public string Username { get; set; }
+
+        [Required(ErrorMessage = "Please enter password")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Display(Name = "Remember me")]
+        public bool RememberMe { get; set; }
+
+        public string ReturnUrl { get; set; }
+    }
+}
---
+++ Homework_22/API+Web+WPF/Web/ViewModels/RegistrationViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Homework_22.ViewModels
+{
+    public class RegistrationViewModel
+    {
+        [Required(ErrorMessage = "Please enter login")]
+        public string Username { get; set; }
+
+        [Required(ErrorMessage = "Please enter password")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Compare(nameof(Password), ErrorMessage = "Password doesn't match")]
+        public string ConfirmPassword { get; set; }
+    }
+}
