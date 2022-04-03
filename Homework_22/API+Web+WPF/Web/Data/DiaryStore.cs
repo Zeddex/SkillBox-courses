a5c5b21d159c87216c8816f372b2d4e681b61b86
Zeddex <zedxlab@gmail.com>
2022-04-03 23:14:34 +0200

    refactoring

---
+++ Homework_22/API+Web+WPF/Web/Data/DiaryStore.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Homework_22_Web.Models;
+
+namespace Homework_22_Web.Data
+{
+    public class DiaryStore
+    {
+        private readonly DiaryContext _db;
+
+        public DiaryStore(DiaryContext db)
+        {
+            _db = db;
+        }
+
+        public async Task<IReadOnlyList<Note>> AllNotesAsync()
+        {
+            return await _db.Notes.ToListAsync();
+        }
+
+        public async Task<Note> GetNoteByIdAsync(int id)
+        {
+            return await _db.Notes.FirstOrDefaultAsync(x => x.Id == id);
+        }
+
+        public async Task AddNoteAsync(Note note)
+        {
+            _db.Notes.Add(note);
+            await _db.SaveChangesAsync();
+        }
+
+        public async Task DeleteNoteAsync(Note note)
+        {
+            _db.Notes.Remove(note);
+            await _db.SaveChangesAsync();
+        }
+
+        public async Task UpdateNoteAsync(Note note)
+        {
+            _db.Notes.Update(note);
+            await _db.SaveChangesAsync();
+        }
+    }
+}
