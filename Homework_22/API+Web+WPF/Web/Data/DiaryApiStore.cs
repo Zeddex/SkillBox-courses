20943f8954444878fcc95be1aade7c77801df32a
Zeddex <zedxlab@gmail.com>
2022-04-05 01:17:01 +0200

    add api

--- Homework_22/API+Web+WPF/Web/Data/DiaryApiStore.cs
+++ Homework_22/API+Web+WPF/Web/Data/DiaryApiStore.cs
@@ -1,43 +1,51 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using Homework_22_Web.Models;
 using System.Net.Http;
+using Newtonsoft.Json;
 
 namespace Homework_22_Web.Data
 {
     public class DiaryApiStore : IDiary
     {
         private readonly HttpClient _httpClient;
+        private const string _apiUrl = @"http://localhost:41444/api/diary";
 
         public DiaryApiStore()
         {
             _httpClient = new HttpClient();
         }
 
-        public void AddNote(Note note)
+        public async Task<List<Note>> AllNotesAsync()
         {
-            throw new System.NotImplementedException();
+            string json = await _httpClient.GetStringAsync(_apiUrl);
+            return JsonConvert.DeserializeObject<List<Note>>(json);
         }
 
-        public IReadOnlyList<Note> AllNotes()
+        public async Task<Note> GetNoteByIdAsync(int id)
         {
-            throw new System.NotImplementedException();
+            string json = await _httpClient.GetStringAsync(_apiUrl + $"//{id}");
+            return JsonConvert.DeserializeObject<Note>(json);
         }
 
-        public void DeleteNote(Note note)
+        public async Task AddNoteAsync(Note note)
         {
-            throw new System.NotImplementedException();
+            var content = new StringContent(JsonConvert.SerializeObject(note), Encoding.UTF8);
+            _ = await _httpClient.PostAsync(_apiUrl, content);
         }
 
-        public Note GetNoteById(int id)
+        public async Task DeleteNoteAsync(int id)
         {
-            throw new System.NotImplementedException();
+            _ = await _httpClient.DeleteAsync(_apiUrl + $"//{id}");
+            
         }
 
-        public void UpdateNote(Note note)
+        public async Task UpdateNoteAsync(Note note)
         {
-            throw new System.NotImplementedException();
+            var content = new StringContent(JsonConvert.SerializeObject(note), Encoding.UTF8);
+            await _httpClient.PutAsync(_apiUrl, content);
         }
     }
 }
