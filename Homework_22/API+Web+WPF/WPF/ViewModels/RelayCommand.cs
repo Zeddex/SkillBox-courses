905ec9c774b76b8b579d744800901b78ac3916be
Zeddex <zedxlab@gmail.com>
2022-04-08 15:33:37 +0200

    wpf started

---
+++ Homework_22/API+Web+WPF/WPF/ViewModels/RelayCommand.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace Homework_22_WPF.ViewModels
+{
+    public class RelayCommand : ICommand
+    {
+        private readonly Action _execute;
+        private readonly Func<object, bool> _canExecute;
+
+        public event EventHandler CanExecuteChanged
+        {
+            add => CommandManager.RequerySuggested += value;
+            remove => CommandManager.RequerySuggested -= value;
+        }
+
+        public RelayCommand(Action execute, Func<object, bool> canExecute = null)
+        {
+            _execute = execute;
+            _canExecute = canExecute;
+        }
+
+        public bool CanExecute(object parameter) => _canExecute == null || _canExecute.Invoke(parameter);
+
+        public void Execute(object parameter) => _execute?.Invoke();
+    }
+}
