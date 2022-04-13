a211c5c02e0d2480a02446ddc2a2903af98ac2f0
Zeddex <zedxlab@gmail.com>
2022-04-13 18:44:45 +0200

    fixes

---
+++ Homework_22/API+Web+WPF/WPF/Infrastructure/Extensions/Extensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Homework_22_WPF.Models;
+
+namespace Homework_22_WPF.Infrastructure.Extensions
+{
+    public static class Extensions
+    {
+        public static ObservableCollection<Note> ToObservableCollection(this IEnumerable<Note> collection)
+        {
+            var oCollection = new ObservableCollection<Note>();
+
+            foreach (var item in collection)
+            {
+                oCollection.Add(item);
+            }
+
+            return oCollection;
+        }
+    }
+}
